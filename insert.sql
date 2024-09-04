249c98caad809f3b57b84743e79b3409fdb365c3
pedrovmcastro <pvmcastro@gmail.com>
2024-09-04 10:21:30 -0300

    update inserts

--- insert.sql
+++ insert.sql
@@ -1,5 +1,6 @@
 -- SCRIPT PARA INSERÇÃO DE DADOS
 
+\! echo 'enderecos';
 -- Inserção de 7 endereços:
 INSERT INTO `enderecos` (`logradouro`, `numero`, `bairro`, `cidade`, `cep`) VALUES
 ('Rua Barão de Jaguara', '123', 'Centro', 'Campinas', '13010-210'),
@@ -10,6 +11,7 @@ INSERT INTO `enderecos` (`logradouro`, `numero`, `bairro`, `cidade`, `cep`) VALU
 ('Rua Dr. Quirino', '303', 'Centro', 'Campinas', '13015-080'),
 ('Avenida Orosimbo Maia', '404', 'Jardim Guanabara', 'Campinas', '13073-003');
 
+\! echo 'usuarios';
 -- Inserção de 4 usuários:
 INSERT INTO `usuarios` (`nome`, `cpf`, `telefone`, `email`) VALUES
 ('Ana Clara Oliveira', '12345678901', '19987654321', 'ana.clara@gmail.com'),
@@ -17,14 +19,16 @@ INSERT INTO `usuarios` (`nome`, `cpf`, `telefone`, `email`) VALUES
 ('Daniela Lima Ferreira', '45678901234', '19987654324', 'daniela.lima@outlook.com'),
 ('Eduardo Lima Ferreira', '56789012345', '19987654325', 'eduardo.lima@gmail.com');
 
+\! echo 'enderecos_usuarios';
 -- Associação entre usuários e endereços:
 INSERT INTO `enderecos_usuarios` (`id_usuario`, `id_endereco`) VALUES
-(1, 5)
-(2, 6)
-(3, 7)
-(4, 7) 
+(1, 5),
+(2, 6),
+(3, 7),
+(4, 7);
 -- Note que o mesmo endereço é atribuído a dois usuários diferentes (pelos nomes provavelmente são irmãos)
 
+\! echo 'lojas';
 -- Iserção de 4 lojas: 
 INSERT INTO `lojas` (`id_endereco`, `nome`, `cnpj`, `ie`, `nota`, `logo`) VALUES
 (1, 'ConstruShopping', '12345678000101', '123456789', 4.5, '/images/logos/construshopping.png'),
@@ -32,6 +36,7 @@ INSERT INTO `lojas` (`id_endereco`, `nome`, `cnpj`, `ie`, `nota`, `logo`) VALUES
 (3, 'Construcenter Campinas', '34567890000123', '345678901', 4.8, '/images/logos/construcenter.png'),
 (4, 'Ferragem e Cia', '45678901000134', '456789012', 4.3, '/images/logos/ferragemecia.png');
 
+\! echo 'veiculos';
 -- Inserção de 5 veículos:
 INSERT INTO `veiculos` (`categoria`, `placa`, `marca`, `modelo`, `imagem`) VALUES
 ('Carro', 'ABC1234', 'Fiat', 'Uno', '/images/veiculos/uno.png'),
@@ -40,13 +45,15 @@ INSERT INTO `veiculos` (`categoria`, `placa`, `marca`, `modelo`, `imagem`) VALUE
 ('Caminhão', 'JKL3456', 'Mercedes-Benz', 'Atego 1419', '/images/veiculos/atego1419.png'),
 ('Moto', 'MNO7890', 'Yamaha', 'Fazer 250', '/images/veiculos/fazer250.png');
 
+\! echo 'motoristas';
 -- Inserção de 4 motoristas:
 INSERT INTO `motoristas` (`id_usuario`, `cnh`, `categoria`, `nota`, `qtd_viagens`, `foto`) VALUES
 (1, '123456789', 'B', 4.8, 150, '/images/motoristas/motorista1.png'),
 (2, '234567890', 'B', 4.6, 120, '/images/motoristas/motorista2.png'),
 (3, '345678901', 'A', 4.9, 180, '/images/motoristas/motorista3.png'),
-(4, '456789012', 'B', 4.7, 100, '/images/motoristas/motorista4.png'),
+(4, '456789012', 'B', 4.7, 100, '/images/motoristas/motorista4.png');
 
+\! echo 'veiculos_motoristas';
 -- Associação entre veículos e motoristas:
 INSERT INTO `veiculos_motoristas` (`id_motorista`, `id_veiculo`) VALUES
 (1, 1),
@@ -56,6 +63,7 @@ INSERT INTO `veiculos_motoristas` (`id_motorista`, `id_veiculo`) VALUES
 (4, 5);
 -- Note que o mesmo motorista está associado a dois veículos
 
+\! echo 'produtos';
 -- Inserção de produtos
 INSERT INTO `produtos` (`id_loja`, `nome`, `descricao`, `preco`, `peso`, `imagem`) VALUES
 (1, 'Cimento Portland', 'Cimento Portland de alta qualidade para construção civil.', 25.50, 50.0, '/images/produtos/cimento_portland.png'),
@@ -71,20 +79,22 @@ INSERT INTO `produtos` (`id_loja`, `nome`, `descricao`, `preco`, `peso`, `imagem
 
 -- Agora vamos simular algumas compras:
 
+\! echo 'carrinhos';
 -- Inserção de carrinhos de produto:
 
 -- Carrinho 1 (Loja 1)
-INSERT INTO `carrinho` (`id_usuario`, `qtd_itens`, `preco_total`, `peso_total`)
+INSERT INTO `carrinhos` (`id_usuario`, `qtd_itens`, `preco_total`, `peso_total`)
 VALUES (1, 2, 132.50, 21.0);
 
 -- Carrinho 2 (Loja 2)
-INSERT INTO `carrinho` (`id_usuario`, `qtd_itens`, `preco_total`, `peso_total`)
+INSERT INTO `carrinhos` (`id_usuario`, `qtd_itens`, `preco_total`, `peso_total`)
 VALUES (2, 2, 122.00, 19.0);
 
 -- Carrinho 3 (Loja 3)
-INSERT INTO `carrinho` (`id_usuario`, `qtd_itens`, `preco_total`, `peso_total`)
+INSERT INTO `carrinhos` (`id_usuario`, `qtd_itens`, `preco_total`, `peso_total`)
 VALUES (3, 3, 150.00, 22.5);
 
+\! echo 'itens_carrinhos';
 -- Adicionando items aos carrinhos:
 
 -- Carrinho 1 - Loja 1
@@ -106,7 +116,8 @@ VALUES
     (3, 6),  -- Produto 6 (Loja 3)
     (3, 7);  -- Produto 7 (Loja 3)
 
--- Inserção dos pedidos
+\! echo 'pedidos';
+-- Inserção dos pedidos:
 
 -- Pedido 1 para Carrinho 1, Entrega no Endereço 5, Usuario 1
 INSERT INTO `pedidos` (`id_carrinho`, `id_endereco_entrega`, `taxa_entrega`, `status_pedido`)
@@ -119,4 +130,66 @@ VALUES (2, 6, 20.00, 'aguardando');
 -- Pedido 3 para Carrinho 3, Entrega no Endereço 7, Usuario 3
 INSERT INTO `pedidos` (`id_carrinho`, `id_endereco_entrega`, `taxa_entrega`, `status_pedido`)
 VALUES (3, 7, 25.00, 'aguardando');
+
+\! echo 'pagamentos';
+-- Inserção dos pagamentos dos pedidos:
+
+INSERT INTO `pagamentos` (`id_pedido`, `metodo_pagamento`, `valor`) VALUES
+(1, 'pix', 157.50),
+(2, 'pix', 134.00),
+(3, 'credito', 165.00);
+
+\! echo 'comissoes_aplicativo';
+-- Inserção das comissões dos pedidos (10% do preço total dos pedidos):
+
+INSERT INTO `comissoes_aplicativo` (`id_pedido`, `valor_comissao`)
+VALUES
+(1, 13.25),
+(2, 12.20),
+(3, 15.00);
+
+\! echo 'entregas';
+-- Inserção das entregas referentes aos pedidos:
+
+-- Entrega para o Pedido 1
+INSERT INTO `entregas` (`id_pedido`, `id_motorista`, `status_entrega`, `codigo_entrega`)
+VALUES (1, 1, 'a caminho', 1234);
+
+-- Entrega para o Pedido 2
+INSERT INTO `entregas` (`id_pedido`, `id_motorista`, `status_entrega`, `codigo_entrega`)
+VALUES (2, 2, 'a caminho', 6543);
+
+-- Entrega para o Pedido 3
+INSERT INTO `entregas` (`id_pedido`, `id_motorista`, `status_entrega`, `codigo_entrega`)
+VALUES (3, 3, 'a caminho', 7890);
+
+\! echo 'pagamentos_motorista';
+-- Inserção do pagamento aos motoristas (80% da taxa de entrega):
+
+-- Pagamento para o Motorista da Entrega 1
+INSERT INTO `pagamentos_motoristas` (`id_entrega`, `valor`)
+VALUES (1, 12.00);
+
+-- Pagamento para o Motorista da Entrega 2
+INSERT INTO `pagamentos_motoristas` (`id_entrega`, `valor`)
+VALUES (2, 16.00);
+
+-- Pagamento para o Motorista da Entrega 3
+INSERT INTO `pagamentos_motoristas` (`id_entrega`, `valor`)
+VALUES (3, 20.00);
+
+\! echo 'taxas_aplicativo';
+-- Inserção das taxas do aplicativo (20% da taxa de entrega):
+
+-- Taxa do Aplicativo para a Entrega 1
+INSERT INTO `taxas_aplicativo` (`id_entrega`, `valor`)
+VALUES (1, 3.00);
+
+-- Taxa do Aplicativo para a Entrega 2
+INSERT INTO `taxas_aplicativo` (`id_entrega`, `valor`)
+VALUES (2, 4.00);
+
+-- Taxa do Aplicativo para a Entrega 3
+INSERT INTO `taxas_aplicativo` (`id_entrega`, `valor`)
+VALUES (3, 5.00);
 
