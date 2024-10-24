7a0236b5cbdb2ccf8a12285f58944b4f83d7f648
pedrovmcastro <pvmcastro@gmail.com>
2024-10-24 20:31:03 -0300

    user model

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+admin.site.register(models.User)
 admin.site.register(models.Endereco)
 admin.site.register(models.Loja)
 admin.site.register(models.Produto)
