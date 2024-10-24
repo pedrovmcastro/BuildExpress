b53a745539f3f07f1aa6d1a5174ee3819c19ac3a
pedrovmcastro <pvmcastro@gmail.com>
2024-10-24 10:37:33 -0300

    add RenamableImageModel

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,39 +1,74 @@
 import os
 from django.db import models
-from django.core.validators import RegexValidator
 
 from . import utils
 
+
+class RenamableImageModel(models.Model):
+    """
+    Classe base para modelos que contêm um campo de imagem que precisa ser renomeado. 
+    """
+    image_field_name = None  # Deve ser definido nas subclasses
+
+    class Meta:
+        abstract = True  # Define que esta é uma classe abstrata, não será criada uma tabela para ela.
+    
+    def save(self, *args, **kwargs):
+        # Primeiro salva o objeto para garantir que ele tenha um ID
+        super().save(*args, **kwargs)
+
+        #Obter o campo de imagem
+        image_field = getattr(self, self.image_field_name)
+
+        # Atualiza o nome da imagem se ela já foi carregada
+        if image_field and not image_field.name.startswith(f"{self.__class__.__name__}{str(self.id).zfill(6)}"):
+            old_path = image_field.path
+            image_field.name = utils.rename_image(self, image_field.name)
+
+            # Salva o objeto novamente para atualizar o caminho no banco de dados
+            super().save(*args, **kwargs)
+
+            # Renomeia o arquivo fisicamente
+            new_path = image_field.path
+            os.rename(old_path, new_path)
+
+
 class Endereco(models.Model):
     logradouro = models.CharField(max_length=100)
-    numero = models.IntegerField()
+    numero = models.IntegerField(null=True, blank=True)
     bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=50)
+    uf = models.CharField(max_length=2, default=None)
     cep = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return f"{self.logradouro}, {self.numero}, {self.bairro}, {self.cidade} - {self.cep}"
 
 
-class Loja(models.Model):
+class Loja(RenamableImageModel):
     nome = models.CharField(max_length=100)
-    cnpj = models.CharField(
-        max_length=14,
-        unique=True,
-        validators=[RegexValidator(regex=r'^\d{14}$', message='CNPJ inválido')]
-    )
-    ie = models.CharField(
-        max_length=14,
-        unique=True,
-        validators=[RegexValidator(regex=r'^\d{9}$', message='IE inválida')]
-    )
+    cnpj = models.CharField(max_length=14)
+    ie = models.CharField(max_length=12)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
     id_endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
-    logo = models.ImageField(upload_to='logos/', null=True, blank=True)
+    logo = models.ImageField(upload_to=utils.rename_image, null=True, blank=True)
+
+    image_field_name = 'logo'
+
+    def __str__(self):
+        return self.nome
 
 
-class Produto(models.Model):
+class Produto(RenamableImageModel):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     peso = models.DecimalField(max_digits=10, decimal_places=2)
     id_loja = models.ForeignKey(Loja, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='produtos/', null=True, blank=True)
+    photo = models.ImageField(upload_to=utils.rename_image, null=True, blank=True)
+
+    image_field_name = 'photo'
+
+    def __str__(self):
+        return self.nome
 
--- ecommerce/utils.py
+++ ecommerce/utils.py
@@ -1,22 +1,24 @@
 import os
 
-def get_image_path(instance, filename, folder):
+def rename_image(instance, filename):
     """
-    Gera o caminho para a imagem com base no tipo da instância (Produto, Loja, etc).
-    
-    Args:
-        instance: A instância do modelo (Produto, Loja, etc).
-        filename: O nome original do arquivo de upload.
-        folder: O nome da pasta onde a imagem será salva (e.g., 'produtos', 'logos').
-    
-    Returns:
-        O caminho completo para onde a imagem será salva.
+    Renomeia o arquivo de imagem baseado no nome da classe do modelo e no ID do objeto.
+    Exemplo: 'Produto000001.jpg' para a model Produto com ID 1, salvando em 'produtos/'.
     """
-    # Extrai a extensão do arquivo original
-    ext = filename.split('.')[-1]
+    # Obtém a extensão do arquivo
+    ext = os.path.splitext(filename)[1]
     
-    # Define o nome do arquivo com o ID da instância formatado
-    filename = f"{instance.__class__.__name__.lower()}{instance.id:06d}.{ext}"
-    
-    # Retorna o caminho completo com o diretório especificado
-    return os.path.join(folder, filename)
+    # Garante que o objeto tenha um ID
+    if instance.id:
+        # Nome do arquivo com base no nome da classe e ID formatado
+        new_filename = f"{instance.__class__.__name__}{str(instance.id).zfill(6)}{ext}"
+        
+        # Define a subpasta (ex: 'produtos/')
+        subfolder = instance.__class__.__name__.lower() + "s/"
+        
+        # Retorna o caminho completo para o diretório desejado
+        return os.path.join(subfolder, new_filename)
+    else:
+        # Se o objeto ainda não tiver um ID, retorna o nome original com a subpasta
+        subfolder = instance.__class__.__name__.lower() + "s/"
+        return os.path.join(subfolder, filename)
