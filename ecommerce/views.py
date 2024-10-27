92c2f737a35e53e39a3d5a444131f817eed05f77
pedrovmcastro <pvmcastro@gmail.com>
2024-10-27 17:18:28 -0300

    update index template

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.db import IntegrityError
 from django.urls import reverse
 
-from .models import Produto, User, Loja, Categoria
+from .models import Produto, User, Loja, Categoria, Wishlist
 
 
 def index(request):
@@ -32,7 +33,7 @@ def login_view(request):
     else:
         return render(request, "ecommerce/login.html")
 
-
+@login_required
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
@@ -130,3 +131,38 @@ def produtos_loja(request, id_loja):
         'produtos': produtos
     })
 
+
+def detalhes_produto(request, id_produto):
+    produto = get_object_or_404(Produto, id=id_produto)
+
+    # Lógica da Wishlist
+    if request.user.is_authenticated:
+        in_wishlist = Wishlist.objects.filter(user=request.user, produto=produto).exists()
+    else:
+        in_wishlist = False
+
+    return render(request, 'ecommerce/detalhes_produto.html', {
+        'produto': produto,
+        'in_wishlist': in_wishlist
+    })
+
+
+@login_required
+def wishlist(request):
+    produtos = Produto.objects.filter(wishlist__user=request.user)
+    return render(request, 'ecommerce/lista_de_desejos.html', {
+        'wishlist': produtos
+    })
+
+
+@login_required
+def acionar_wishlist(request, id_produto):
+    produto = get_object_or_404(Produto, id=id_produto)
+    wishlist_item = Wishlist.objects.filter(user=request.user, produto=produto).first()
+
+    if wishlist_item:
+        wishlist_item.delete()
+    else:
+        Wishlist.objects.create(user=request.user, produto=produto)
+    
+    return redirect('detalhes_produto', id_produto)
