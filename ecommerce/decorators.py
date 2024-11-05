c81953f9acd78a90130315efae117330966b609f
pedrovmcastro <pvmcastro@gmail.com>
2024-11-05 15:17:26 -0300

    normalização dos decorators

--- ecommerce/decorators.py
+++ ecommerce/decorators.py
@@ -7,7 +7,7 @@ def usuario_comum_required(view_func):
     @wraps(view_func)
     @login_required  # Garantir que o usuário esteja logado
     def _wrapped_view(request, *args, **kwargs):
-        if hasattr(request.user, 'usuariocomum') and not request.user.is_lojista and not request.user.is_motorista:
+        if not request.user.is_lojista and not request.user.is_motorista:
             return view_func(request, *args, **kwargs)
         else:
             raise PermissionDenied  # Retorna erro 403 se não for um usuário comum
