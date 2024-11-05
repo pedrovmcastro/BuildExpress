e59988ca93180fd905afa9ad8bf6e54644be90af
pedrovmcastro <pvmcastro@gmail.com>
2024-11-05 10:17:44 -0300

    regularização do acesso ao admin a lojistas e motoristas

--- entregas/models.py
+++ entregas/models.py
@@ -24,10 +24,18 @@ class Motorista(AbstractBaseUser):
     cnh = models.CharField(max_length=15, unique=True)
     nota = models.FloatField(default=None, null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Para permitir acesso ao admin
+    is_superuser = models.BooleanField(default=False)  # Para permitir permissões de superusuário
 
     USERNAME_FIELD = 'email'
 
     objects = MotoristaManager()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+    
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
     def __str__(self):
         return self.email
